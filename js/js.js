28e1100fa3512f81d550faa2657b711789d9d503
BlessCrista11 <blesscrista11@gmail.com>
2020-09-19 11:43:09 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,97 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".carousel_1");
+  owl.owlCarousel({
+    items: 1,
+    margin: 0,
+    loop: true,
+    nav: false,
+  dots: true,
+  });
+});
+
+// $('.owl-carousel_1').owlCarousel({
+//     loop:true,
+//     nav: true,
+//     margin:10,
+//     // responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:2,
+//             nav:false
+//         },
+//         1000:{
+//             items:4,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
+
+// $(document).ready(function(){
+//   $(".owl-carousel_1").owlCarousel({
+//   	loop:true,
+//     nav: true,
+//     margin:10,
+//     // responsiveClass:true,
+//   		responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:2,
+//             nav:false
+//         },
+//         1000:{
+//             items:4,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+//   });
+// });
+
+// $(document).ready(function() {
+// 	    $('.carousel_1').owlCarousel({
+// 	        loop:true,
+// 	        margin:10,
+// 	        nav:true,
+// 	        dots: true,	        
+// 	        responsive:{
+// 	            0:{
+// 	                items:1
+// 	            },
+// 	            600:{
+// 	                items:2
+// 	            },
+// 	            1000:{
+// 	                items:4
+// 	            }
+// 	        }
+// 	    });
+// 	});
+
+$(document).ready(function() {
+	    $('.carousel_2').owlCarousel({
+	        loop:true,
+	        margin:10,
+	        nav:true,
+	        dots: false,	        
+	        responsive:{
+	            0:{
+	                items:1
+	            },
+	            600:{
+	                items:2
+	            },
+	            1000:{
+	                items:4
+	            }
+	        }
+	    });
+	});
